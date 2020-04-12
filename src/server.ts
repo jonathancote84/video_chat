f8c75ede80c0a83bdb38f2a095f4de1a5dd6e719
Jonathan Cote <jonathancote92@gmail.com>
2020-04-12 11:25:27 -0500

    workin fix

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { ChatServer } from './chat-server';
 let app = new ChatServer().getApp();
 
 app.get('/', (request, response) => {
-    response.send('Hello world!');
+    response.send('Hello Good ol friend');
 });
 
-app.listen(5000);
+export { app };
